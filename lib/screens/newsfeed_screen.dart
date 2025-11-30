80cf77996d9218f5c2815535a9e1282966967fef
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-11-30 12:16:47 +0800

    Add screens for display

--- lib/screens/newsfeed_screen.dart
+++ lib/screens/newsfeed_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import '../widgets/newsfeed_card.dart';
 
 class NewsfeedScreen extends StatelessWidget {
   const NewsfeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'News Feed'
+    return ListView(
+      children: const [
+        NewsFeedCard(
+          userName: 'Mackenzie Iguiron',
+          postContent: 'Kamusta',
+          numOfLikes: 100,
+          date: 'November 29',
+        ),
+        NewsFeedCard(
+          userName: 'Vergel Santiago',
+          postContent: 'Hello everyone',
+          numOfLikes: 210,
+          hasImage: true,
+          date: 'November 30',
+        ),
+      ],
     );
   }
 }
