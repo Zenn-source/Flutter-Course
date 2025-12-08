99a52ec89fd49488e29e557c410a06c565f1edb1
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-12-08 18:44:37 +0800

    feat: Make notifications dynamic using ListView

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -3,24 +3,57 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NotificationScreen extends StatefulWidget {
-  const NotificationScreen({
-    super.key
-  });
+  const NotificationScreen({super.key});
   @override
   State<NotificationScreen> createState() => _NotificationScreenState();
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
+  final List<Map<String, String>> notifications = [
+    {
+      'name': 'Lebron James',
+      'post': 'commented on your post.',
+      'description': '"Nice work on the Flutter project!"',
+    },
+    {
+      'name': 'Tyler',
+      'post': 'liked your photo.',
+      'description': 'Your photo reached 167 likes.',
+    },
+    {
+      'name': 'Nicole',
+      'post': 'shared your video.',
+      'description': 'Click to see the share.',
+    },
+    {
+      'name': 'Security Alert',
+      'post': 'New login detected.',
+      'description': 'Was this you?',
+    },
+    {
+      'name': 'Event Reminder',
+      'post': 'invited you to "Code Camp".',
+      'description': 'Tomorrow at 10:00 AM.',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
       width: ScreenUtil().screenWidth,
-      child: const Column(
-        children: [
-          notif.Notification(name: 'Test', post: 'Post', description: 'Description',),
-          Divider(),
-        ],
+      child: ListView.separated(
+        itemCount: notifications.length, 
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, index) {
+          final notification = notifications[index];
+
+          return notif.Notification(
+            name: notification['name']!,
+            post: notification['post']!,
+            description: notification['description']!,
+          );
+        },
       ),
     );
   }
