a0dbafa44bbce7c68ae6bb85f11e2ccdd85c31a0
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-12-09 10:09:43 +0800

    unfinished

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iguiron_mobprog/widgets/custom_font.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Container(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      image: const DecorationImage(
+                        image: NetworkImage(''),
+                        fit: BoxFit.cover,
+                      )
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -50,
+                    left: ScreenUtil().setWidth(20),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(''),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: CircleAvatar(
+                            radius: 15,
+                            backgroundColor: Colors.grey[300],
+                            child: const Icon(
+                              Icons.camera_alt,
+                              size: 16,
+                              color: Colors.black,
+                            ),
+                          )
+                        )
+                      ],
+                    )
+                  )
+                ],
+              ),
+              SizedBox(height: ScreenUtil().setHeight(55)),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(20)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomFont(text: 'Mackenzie', fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(20), color: Colors.black,),
+                  SizedBox(height: ScreenUtil().setHeight(5)),
+                  Row(
+                    children: [
+                      CustomFont(text: '1M', fontSize: ScreenUtil().setSp(15), color: Colors.black, fontWeight: FontWeight.bold,),
+                      SizedBox(width: ScreenUtil().setWidth(10)),
+                      CustomFont(text: 'followers', fontSize: ScreenUtil().setSp(15), color: Colors.grey, fontWeight: FontWeight.w100,),
+                      SizedBox(width: ScreenUtil().setWidth(5)),
+                      Icon(
+                        Icons.circle,
+                        size: ScreenUtil().setSp(5),
+                        color: Colors.grey,
+                      ),
+                      SizedBox(width: ScreenUtil().setWidth(5)),
+                      CustomFont(text: '1', fontSize: ScreenUtil().setSp(15), color: Colors.black, fontWeight: FontWeight.bold,),
+                      SizedBox(width: ScreenUtil().setWidth(10)),
+                      CustomFont(text: 'following', fontSize: ScreenUtil().setSp(15), color: Colors.grey, fontWeight: FontWeight.w100,),
+                    ],
+                  ),
+                  SizedBox(height: ScreenUtil().setHeight(10)),
+                  Row()
+                  ],
+                ),
+              )
+            ],
+          )
+        )
+      )
+    )
+  }
+}
