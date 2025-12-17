096f32b0a2a495654c90cf7a884f98ab6573bb08
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-12-17 22:16:47 +0800

    feat: add detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -10,7 +10,7 @@ class DetailScreen extends StatelessWidget {
   final String postContent;
   final String date;
   int numOfLikes;
-  final String? contentImage;
+  final String contentImage;
 
   DetailScreen({
     super.key,
@@ -21,5 +21,134 @@ class DetailScreen extends StatelessWidget {
     required this.date,
     this.contentImage = '',
   });
-  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: CustomFont(
+          text: userName,
+          fontSize: ScreenUtil().setSp(20),
+          color: Colors.black,
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        height: ScreenUtil().screenHeight,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              (contentImage == '')
+                  ? SizedBox(height: ScreenUtil().setHeight(0))
+                  : Image.network(contentImage),
+              SizedBox(height: ScreenUtil().setHeight(20)),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: ScreenUtil().setWidth(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    (userImage == '')
+                        ? const Icon(Icons.person)
+                        : CircleAvatar(
+                            radius: ScreenUtil().setSp(25),
+                            backgroundImage: NetworkImage(contentImage),
+                          ),
+                    SizedBox(width: ScreenUtil().setWidth(10)),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CustomFont(
+                          text: userName,
+                          fontSize: ScreenUtil().setSp(20),
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            CustomFont(
+                              text: date,
+                              fontSize: ScreenUtil().setSp(15),
+                              color: Colors.grey,
+                            ),
+                            SizedBox(width: ScreenUtil().setWidth(3)),
+                            Icon(
+                              Icons.public,
+                              color: Colors.grey,
+                              size: ScreenUtil().setSp(18),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Icon(Icons.more_horiz),
+                  ],
+                ),
+              ),
+              SizedBox(height: ScreenUtil().setHeight(15)),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: ScreenUtil().setWidth(20),
+                ),
+                alignment: Alignment.centerLeft,
+                child: CustomFont(
+                  text: postContent,
+                  fontSize: ScreenUtil().setSp(18),
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: ScreenUtil().setHeight(30)),
+              Divider(),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: ScreenUtil().setWidth(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () {
+                        print('Licked');
+                      },
+                      icon: const Icon(Icons.thumb_up, color: FB_DARK_PRIMARY),
+                      label: CustomFont(
+                        text: (numOfLikes == 0)
+                            ? 'Like'
+                            : numOfLikes.toString(),
+                        fontSize: ScreenUtil().setSp(12),
+                        color: FB_DARK_PRIMARY,
+                      ),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.comment, color: FB_DARK_PRIMARY),
+                      label: CustomFont(
+                        text: 'Comment',
+                        fontSize: ScreenUtil().setSp(12),
+                        color: FB_DARK_PRIMARY,
+                      ),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.comment, color: FB_DARK_PRIMARY),
+                      label: CustomFont(
+                        text: 'Share',
+                        fontSize: ScreenUtil().setSp(12),
+                        color: FB_DARK_PRIMARY,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
