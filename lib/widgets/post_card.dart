710925d5149a0990e0f79b142b7a93e1f956c12f
Zenn-source <mackenzie.g.iguiron@gmail.com>
2026-01-03 23:31:26 +0800

    Make posts clickable

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../constants.dart';
 import 'custom_font.dart';
+import '../screens/detail_screen.dart';
 
 class ActionButton extends StatelessWidget {
   final IconData icon;
@@ -30,8 +31,7 @@ class ActionButton extends StatelessWidget {
     );
   }
 }
-
-class PostCard extends StatelessWidget {
+class PostCard extends StatefulWidget {
   final String userName;
   final String userImage;
   final String postContent;
@@ -50,6 +50,48 @@ class PostCard extends StatelessWidget {
   });
 
   @override
+  State<PostCard> createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  late int _currentLikes;
+  bool _isLiked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentLikes = widget.numOfLikes;
+  }
+
+  void _toggleLike() {
+    setState(() {
+      if (_isLiked) {
+        _currentLikes--;
+        _isLiked = false;
+      } else {
+        _currentLikes++;
+        _isLiked = true;
+      }
+    });
+  }
+
+  void _navigateToDetail() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailScreen(
+          userName: widget.userName,
+          userImage: widget.userImage,
+          postContent: widget.postContent,
+          numOfLikes: _currentLikes,
+          date: widget.date,
+          contentImage: widget.contentImage ?? '',
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(ScreenUtil().setSp(10)),
@@ -58,81 +100,88 @@ class PostCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  radius: 20,
-                  backgroundImage: AssetImage(userImage), 
-                ),
-                SizedBox(
-                  width: ScreenUtil().setWidth(10),
-                ),
-                Column(
+            GestureDetector(
+              onTap: _navigateToDetail,
+              child: Container(
+                color: Colors
+                    .transparent,
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    CustomFont(
-                      text: userName,
-                      fontSize: ScreenUtil().setSp(15),
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        CustomFont(
-                          text: date,
-                          fontSize: ScreenUtil().setSp(12),
-                          color: Colors.grey,
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundImage: AssetImage(widget.userImage),
                         ),
-                        SizedBox(
-                          width: ScreenUtil().setWidth(3),
+                        SizedBox(width: ScreenUtil().setWidth(10)),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomFont(
+                              text: widget.userName,
+                              fontSize: ScreenUtil().setSp(15),
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            Row(
+                              children: [
+                                CustomFont(
+                                  text: widget.date,
+                                  fontSize: ScreenUtil().setSp(12),
+                                  color: Colors.grey,
+                                ),
+                                SizedBox(width: ScreenUtil().setWidth(3)),
+                                Icon(
+                                  Icons.public,
+                                  color: Colors.grey,
+                                  size: ScreenUtil().setSp(15),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                        Icon(
-                          Icons.public,
-                          color: Colors.grey,
-                          size: ScreenUtil().setSp(15),
-                        ),
+                        const Spacer(),
+                        const Icon(Icons.more_horiz),
                       ],
                     ),
+                    SizedBox(height: ScreenUtil().setHeight(5)),
+                    // Post Content
+                    CustomFont(
+                      text: widget.postContent,
+                      fontSize: ScreenUtil().setSp(12),
+                      color: Colors.black,
+                    ),
+                    SizedBox(height: ScreenUtil().setHeight(5)),
+                    // Content Image
+                    widget.contentImage != null
+                        ? Image.asset(
+                            widget.contentImage!,
+                            height: ScreenUtil().setHeight(350),
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          )
+                        : const SizedBox(),
                   ],
                 ),
-                Spacer(),
-                Icon(Icons.more_horiz),
-              ],
+              ),
             ),
 
-            SizedBox(height: ScreenUtil().setHeight(5)),
-            // post content
-            CustomFont(
-              text: postContent,
-              fontSize: ScreenUtil().setSp(12),
-              color: Colors.black,
-            ),
-            SizedBox(height: ScreenUtil().setHeight(5)),
-            contentImage != null
-            ? Image.asset(
-                contentImage!,
-                height: ScreenUtil().setHeight(350), 
-                width: double.infinity,
-                fit: BoxFit.cover,
-              )     
-            : const SizedBox(),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 ActionButton(
                   icon: Icons.thumb_up,
-                  label: '$numOfLikes', 
-                  color: FB_DARK_PRIMARY,
-                  onPressed: () => print("Liked"),
+                  label: '$_currentLikes',
+                  color: _isLiked ? Colors.blue : FB_DARK_PRIMARY,
+                  onPressed: _toggleLike,
                 ),
                 ActionButton(
                   icon: Icons.comment,
                   label: 'Comment',
                   color: FB_DARK_PRIMARY,
-                  onPressed: () {},
+                  onPressed: _navigateToDetail,
                 ),
                 ActionButton(
                   icon: Icons.redo,
@@ -142,15 +191,14 @@ class PostCard extends StatelessWidget {
                 ),
               ],
             ),
+
             Row(
               children: [
                 const CircleAvatar(
                   radius: 13,
                   backgroundImage: AssetImage('assets/images/me.jpg'),
                 ),
-                SizedBox(
-                  width: ScreenUtil().setWidth(10),
-                ),
+                SizedBox(width: ScreenUtil().setWidth(10)),
                 Container(
                   padding: EdgeInsets.fromLTRB(ScreenUtil().setSp(10), 0, 0, 0),
                   alignment: Alignment.centerLeft,
@@ -159,24 +207,26 @@ class PostCard extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: Colors.grey[200],
                     borderRadius: BorderRadius.all(
-                      Radius.circular(ScreenUtil().setSp(10))),
+                      Radius.circular(ScreenUtil().setSp(10)),
                     ),
+                  ),
                   child: CustomFont(
-                  text: 'Write a comment...',
-                  fontSize: ScreenUtil().setSp(11),
-                  color: Colors.grey
-                  ),  
+                    text: 'Write a comment...',
+                    fontSize: ScreenUtil().setSp(11),
+                    color: Colors.grey,
+                  ),
                 ),
               ],
             ),
-            SizedBox(
-              height: ScreenUtil().setHeight(10),
-            ),
-            CustomFont(
-              text: 'View comments',
-              fontSize: ScreenUtil().setSp(12),
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+            SizedBox(height: ScreenUtil().setHeight(10)),
+            GestureDetector(
+              onTap: _navigateToDetail,
+              child: CustomFont(
+                text: 'View comments',
+                fontSize: ScreenUtil().setSp(12),
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
           ],
         ),
@@ -184,4 +234,3 @@ class PostCard extends StatelessWidget {
     );
   }
 }
-
