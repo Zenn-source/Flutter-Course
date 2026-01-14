0ba8098da7f784aba770065d2b5d8f151b27fdf3
Zenn-source <mackenzie.g.iguiron@gmail.com>
2026-01-14 11:32:15 +0800

    Update main.dart to handle login and register routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iguiron_mobprog/screens/home_screen.dart';
+import 'package:iguiron_mobprog/screens/login_screen.dart';
 import 'package:iguiron_mobprog/screens/newsfeed_screen.dart';
 import 'package:iguiron_mobprog/screens/notification_screen.dart';
+import 'package:iguiron_mobprog/screens/register_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,15 +19,17 @@ class MainApp extends StatelessWidget {
       designSize: const Size(412, 715),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) {
+      builder: (_, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Iguiron Facebook',
-          initialRoute: '/home',
+          initialRoute: '/login',
           routes: {
             '/newsfeed': (context) => const NewsfeedScreen(),
             '/home': (context) => const HomeScreen(),
             '/notifications': (context) => const NotificationScreen(),
+            '/login': (context) => const LogInScreen(),
+            'register': (context)=> const RegisterScreen(),
           },
         );
       },
