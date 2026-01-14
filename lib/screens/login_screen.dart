2d56d42ae1d3f666a69dbd2c12dda1b0b1d2ccfa
Zenn-source <mackenzie.g.iguiron@gmail.com>
2026-01-14 11:32:43 +0800

    Finish base login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -40,7 +40,7 @@ class _LogInScreenState extends State<LogInScreen> {
                   child: Column(
                     children: [
                       Image.asset(
-                        'assets/images/NUCCIT Logo_Black.png',
+                        'assets/images/NUCCITLogo_Black.png',
                         height: ScreenUtil().setHeight(200),
                       ),
                       SizedBox(
@@ -60,17 +60,65 @@ class _LogInScreenState extends State<LogInScreen> {
                       SizedBox(
                         height: ScreenUtil().setHeight(10),
                       ),
-
-                      //CONTINUATION
+                      CustomTextFormField(
+                        height: ScreenUtil().setHeight(10),
+                        width: ScreenUtil().setWidth(10),
+                        controller: passwordController,
+                        isObscure: true,
+                        validator: (value) => value!.isEmpty ? 'Enter your password' : null,
+                        onSaved: (value) => passwordController = value!,
+                        fontSize: ScreenUtil().setSp(15),
+                        fontColor: FB_DARK_PRIMARY,
+                        hintTextSize: ScreenUtil().setSp(15),
+                        hintText: 'Password',
+                      ),
+                      SizedBox(height: ScreenUtil().setHeight(50)),
+                      CustomInkwellButton(
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
+                          }
+                        },
+                        height: ScreenUtil().setHeight(40),
+                        width: ScreenUtil().screenWidth,
+                        buttonName: 'Login',
+                        fontSize: ScreenUtil().setSp(15),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: ScreenUtil().screenWidth,
+                  height: ScreenUtil().setHeight(40),
+                  color: FB_DARK_PRIMARY,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('You do not have an account?', 
+                        style: TextStyle(
+                          color: Colors.grey.shade200,
+                          fontSize: ScreenUtil().setSp(15)
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () => Navigator.popAndPushNamed(context, 'register'),
+                        child: Text(
+                          'Register here',
+                          style: TextStyle(
+                            color: FB_LIGHT_PRIMARY,
+                            fontSize: ScreenUtil().setSp(15),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 )
               ],
-            )
+            ),
           ),
-        )
-      )
-    )
+        ),
+      ),
+    );
   }
-  
 }
