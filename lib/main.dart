d4a420556157878be21b6bb10a121f1ee0779ed5
Zenn-source <mackenzie.g.iguiron@gmail.com>
2026-01-19 17:44:06 +0800

    Update main.dart routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iguiron_mobprog/screens/home_screen.dart';
-import 'package:iguiron_mobprog/screens/login_screen.dart';
+import 'package:iguiron_mobprog/screens/login_screen.dart'; // Import Login
 import 'package:iguiron_mobprog/screens/newsfeed_screen.dart';
 import 'package:iguiron_mobprog/screens/notification_screen.dart';
-import 'package:iguiron_mobprog/screens/register_screen.dart';
+import 'package:iguiron_mobprog/screens/register_screen.dart'; // 1. IMPORT THIS
 
 void main() {
   runApp(const MainApp());
@@ -19,20 +19,21 @@ class MainApp extends StatelessWidget {
       designSize: const Size(412, 715),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (_, child) {
+      builder: (context, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Iguiron Facebook',
           initialRoute: '/login',
           routes: {
+            '/login': (context) => const LogInScreen(),
+            '/register': (context) =>
+                const RegisterScreen(),
             '/newsfeed': (context) => const NewsfeedScreen(),
             '/home': (context) => const HomeScreen(),
             '/notifications': (context) => const NotificationScreen(),
-            '/login': (context) => const LogInScreen(),
-            'register': (context)=> const RegisterScreen(),
           },
         );
       },
-    ); 
+    );
   }
 }
