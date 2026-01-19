3c915f20f48d6c6574ea55cd9569675a83d4c90f
Zenn-source <mackenzie.g.iguiron@gmail.com>
2026-01-19 18:02:51 +0800

    feat: Modify custom_textformfield to handle the suffixIcon

--- lib/widgets/custom_textformfield.dart
+++ lib/widgets/custom_textformfield.dart
@@ -1,40 +1,39 @@
 import 'package:flutter/services.dart';
-
 import '../constants.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  CustomTextFormField(
-    {
-      super.key,
-      required this.validator,
-      required this.onSaved,
-      this.controller = TextEditingController,
-      this.isObscure = false,
-      required this.fontSize,
-      required this.fontColor,
-      this.hintTextSize = 12,
-      this.hintText = '',
-      this.fillColor = Colors.black12,
-      required this.height,
-      required this.width,
-      this.keyboardType = TextInputType.text,
-      this.maxLength = 200
-    }
-  );
+  const CustomTextFormField({
+    super.key,
+    required this.validator,
+    required this.onSaved,
+    this.controller,
+    this.isObscure = false,
+    required this.fontSize,
+    required this.fontColor,
+    this.hintTextSize = 12,
+    this.hintText = '',
+    this.fillColor = Colors.black12,
+    required this.height,
+    required this.width,
+    this.keyboardType = TextInputType.text,
+    this.maxLength = 200,
+    this.suffixIcon,
+  });
 
-  final validator;
-  final onSaved;
-  final controller;
-  final isObscure;
-  final fontSize;
-  final fontColor;
+  final String? Function(String?)? validator;
+  final void Function(String?)? onSaved;
+  final TextEditingController? controller;
+  final bool isObscure;
+  final double fontSize;
+  final Color? fontColor;
   final double height, width;
-  final hintTextSize;
-  final hintText;
-  final fillColor;
-  TextInputType keyboardType;
-  int maxLength;
+  final double hintTextSize;
+  final String hintText;
+  final Color fillColor;
+  final TextInputType keyboardType;
+  final int maxLength;
+  final Widget? suffixIcon;
 
   @override
   Widget build(BuildContext context) {
@@ -44,52 +43,28 @@ class CustomTextFormField extends StatelessWidget {
       controller: controller,
       obscureText: isObscure,
       keyboardType: keyboardType,
-      inputFormatters: [
-        LengthLimitingTextInputFormatter(maxLength),
-      ],
-      style: TextStyle(
-        fontSize: fontSize,
-        color: fontColor,
-      ),
+      inputFormatters: [LengthLimitingTextInputFormatter(maxLength)],
+      style: TextStyle(fontSize: fontSize, color: fontColor),
       decoration: InputDecoration(
+        suffixIcon: suffixIcon,
         contentPadding: EdgeInsets.fromLTRB(width, height, width, height),
         focusColor: Colors.black12,
         enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: FB_DARK_PRIMARY,
-            width: 2,
-          ),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
-          ),
+          borderSide: BorderSide(color: FB_DARK_PRIMARY, width: 2),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
         errorBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.red,
-            width: 2,
-          ),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
-          ),
+          borderSide: BorderSide(color: Colors.red, width: 2),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
         errorStyle: const TextStyle(fontFamily: 'Frutiger'),
         focusedErrorBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.red,
-            width: 2,
-          ),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
-          ),
+          borderSide: BorderSide(color: Colors.red, width: 2),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
         focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: FB_LIGHT_PRIMARY,
-            width: 2,
-          ),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
-          ),
+          borderSide: BorderSide(color: FB_LIGHT_PRIMARY, width: 2),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
         filled: true,
         hintStyle: TextStyle(
@@ -98,8 +73,8 @@ class CustomTextFormField extends StatelessWidget {
           fontFamily: 'Frutiger',
         ),
         hintText: hintText,
-        fillColor: fillColor
-      ), 
+        fillColor: fillColor,
+      ),
     );
   }
 }
