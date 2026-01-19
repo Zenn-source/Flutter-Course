9036b2d6efa8093fc6457c05a0872bde1ede60f3
Zenn-source <mackenzie.g.iguiron@gmail.com>
2026-01-19 18:14:04 +0800

    feat: handle images for notification icons

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
+
   @override
   State<NotificationScreen> createState() => _NotificationScreenState();
 }
@@ -14,63 +15,87 @@ class _NotificationScreenState extends State<NotificationScreen> {
       'name': 'Lebron James',
       'post': 'commented on your post.',
       'description': '"Nice work on the Flutter project!"',
+      'image':
+          'assets/images/LJ.png',
     },
     {
       'name': 'Tyler',
       'post': 'liked your photo.',
       'description': 'Your photo reached 167 likes.',
+      'image': 'assets/images/tyler.png',
     },
     {
       'name': 'Nicole',
       'post': 'shared your video.',
       'description': 'Click to see the share.',
+      'image': 'assets/images/nicole.png',
     },
     {
       'name': 'Security Alert',
       'post': 'New login detected.',
       'description': 'Was this you?',
+      'image': 'assets/images/security.png',
     },
     {
       'name': 'Event Reminder',
       'post': 'invited you to Notion Meeting.',
       'description': 'December 13 at 4:00 PM.',
+      'image': 'assets/images/calendar.png',
     },
     {
       'name': 'New login detected',
       'post': 'at Manila, Philippines.',
       'description': 'Yesterday at 9:00 PM.',
+      'image': 'assets/images/security.png',
     },
     {
-      'name': 'Ronald marked safe',
-      'post': 'at Sta. Cruz, Zambales',
-      'description': '12:32 AM.',
+      'name': 'New login detected',
+      'post': 'at Manila, Philippines.',
+      'description': 'Yesterday at 9:00 PM.',
+      'image': 'assets/images/security.png',
     },
     {
-      'name': 'Ronald marked safe',
-      'post': 'at Sta. Cruz, Zambales',
-      'description': '12:32 AM.',
+      'name': 'New login detected',
+      'post': 'at Manila, Philippines.',
+      'description': 'Yesterday at 9:00 PM.',
+      'image': 'assets/images/security.png',
     },
     {
-      'name': 'Ronald marked safe',
-      'post': 'at Sta. Cruz, Zambales',
-      'description': '12:32 AM.',
+      'name': 'New login detected',
+      'post': 'at Manila, Philippines.',
+      'description': 'Yesterday at 9:00 PM.',
+      'image': 'assets/images/security.png',
     },
     {
-      'name': 'Ronald marked safe',
-      'post': 'at Sta. Cruz, Zambales',
-      'description': '12:32 AM.',
+      'name': 'New login detected',
+      'post': 'at Manila, Philippines.',
+      'description': 'Yesterday at 9:00 PM.',
+      'image': 'assets/images/security.png',
     },
     {
-      'name': 'Ronald marked safe',
-      'post': 'at Sta. Cruz, Zambales',
-      'description': '12:32 AM.',
+      'name': 'New login detected',
+      'post': 'at Manila, Philippines.',
+      'description': 'Yesterday at 9:00 PM.',
+      'image': 'assets/images/security.png',
     },
     {
-      'name': 'Ronald marked safe',
-      'post': 'at Sta. Cruz, Zambales',
-      'description': '12:32 AM.',
+      'name': 'New login detected',
+      'post': 'at Manila, Philippines.',
+      'description': 'Yesterday at 9:00 PM.',
+      'image': 'assets/images/security.png',
     },
-    
+    {
+      'name': 'New login detected',
+      'post': 'at Manila, Philippines.',
+      'description': 'Yesterday at 9:00 PM.',
+      'image': 'assets/images/security.png',
+    },
+    {
+      'name': 'New login detected',
+      'post': 'at Manila, Philippines.',
+      'description': 'Yesterday at 9:00 PM.',
+      'image': 'assets/images/security.png',
+    },
   ];
 
   @override
@@ -79,15 +104,16 @@ class _NotificationScreenState extends State<NotificationScreen> {
       color: Colors.white,
       width: ScreenUtil().screenWidth,
       child: ListView.separated(
-        itemCount: notifications.length, 
+        itemCount: notifications.length,
         separatorBuilder: (context, index) => const Divider(),
         itemBuilder: (context, index) {
           final notification = notifications[index];
 
           return info.CustomInformation(
-            name: notification['name']!,    
+            name: notification['name']!,
             post: notification['post']!,
             description: notification['description']!,
+            imagePath: notification['image']!, 
           );
         },
       ),
