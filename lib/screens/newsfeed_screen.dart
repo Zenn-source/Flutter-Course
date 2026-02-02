0b03ae8b832e8d177f14fe8cc1e425a0480e31c0
Zenn-source <mackenzie.g.iguiron@gmail.com>
2026-02-02 17:17:25 +0800

    feat: add advertisements after every post - users can swipe left/rigth - cached loading

--- lib/screens/newsfeed_screen.dart
+++ lib/screens/newsfeed_screen.dart
@@ -1,4 +1,3 @@
-// lib/screens/newsfeed_screen.dart
 import 'package:flutter/material.dart';
 import '../widgets/post_card.dart';
 
@@ -14,7 +13,24 @@ class NewsfeedScreen extends StatelessWidget {
         'content': 'Just finished the mobile dev project!',
         'likes': 167,
         'date': 'December 12',
-        'contentImage': null, 
+        'contentImage': null,
+        'isAd': false,
+      },
+      {
+        'name': 'Tropical Paradise Resort',
+        'userImage': 'assets/images/resort_logo.jpg',
+        'content': 'Escape to paradise! Book your dream vacation today. 🏝️',
+        'likes': 892,
+        'date': 'Sponsored',
+        'contentImage': null,
+        'carouselImages': [
+          'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+          'https://images.unsplash.com/photo-1540541338287-41700207dee6?w=800',
+          'https://images.unsplash.com/photo-1571896349842-33c89424de2d?w=800',
+          'https://images.unsplash.com/photo-1540541338287-41700207dee6?w=800',
+          'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        ],
+        'isAd': true,
       },
       {
         'name': 'Mackenzie Iguiron',
@@ -23,6 +39,23 @@ class NewsfeedScreen extends StatelessWidget {
         'likes': 210,
         'date': 'December 7',
         'contentImage': 'assets/images/mtfuji.jpg',
+        'isAd': false,
+      },
+      {
+        'name': 'Tropical Paradise Resort',
+        'userImage': 'assets/images/resort_logo.jpg',
+        'content': 'Escape to paradise! Book your dream vacation today. 🏝️',
+        'likes': 892,
+        'date': 'Sponsored',
+        'contentImage': null,
+        'carouselImages': [
+          'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+          'https://images.unsplash.com/photo-1540541338287-41700207dee6?w=800',
+          'https://images.unsplash.com/photo-1571896349842-33c89424de2d?w=800',
+          'https://images.unsplash.com/photo-1540541338287-41700207dee6?w=800',
+          'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        ],
+        'isAd': true,
       },
       {
         'name': 'Lebron James',
@@ -31,6 +64,7 @@ class NewsfeedScreen extends StatelessWidget {
         'likes': 45,
         'date': 'December 6',
         'contentImage': 'assets/images/lebron!.png',
+        'isAd': false,
       },
       {
         'name': 'Tyler',
@@ -39,6 +73,24 @@ class NewsfeedScreen extends StatelessWidget {
         'likes': 6734,
         'date': 'December 4',
         'contentImage': 'assets/images/tylercon.jpg',
+        'isAd': false,
+      },
+      {
+        'name': 'TechGear Pro',
+        'userImage': 'assets/images/tech_logo.jpg',
+        'content':
+            'New wireless earbuds - 50% off this week only! Premium sound quality.',
+        'likes': 1203,
+        'date': 'Sponsored',
+        'contentImage': null,
+        'carouselImages': [
+          'https://images.unsplash.com/photo-1590658268037-6bf12165a8df?w=800',
+          'https://images.unsplash.com/photo-1606220945770-b5b6c2c55bf1?w=800',
+          'https://images.unsplash.com/photo-1484704849700-f032a568e944?w=800',
+          'https://images.unsplash.com/photo-1606220945770-b5b6c2c55bf1?w=800',
+          'https://images.unsplash.com/photo-1590658268037-6bf12165a8df?w=800',
+        ],
+        'isAd': true,
       },
       {
         'name': 'Vergel',
@@ -47,14 +99,34 @@ class NewsfeedScreen extends StatelessWidget {
         'likes': 32,
         'date': 'December 2',
         'contentImage': 'assets/images/cats.jpg',
+        'isAd': false,
+      },
+      {
+        'name': 'FitLife Gym',
+        'userImage': 'assets/images/gym_logo.jpg',
+        'content':
+            'Transform your body! Join now and get 1 month free. Limited slots available.',
+        'likes': 567,
+        'date': 'Sponsored',
+        'contentImage': null,
+        'carouselImages': [
+          'https://images.unsplash.com/photo-1534438327276-14e5300c3a48?w=800',
+          'https://images.unsplash.com/photo-1571902943202-507ec2618e8f?w=800',
+          'https://images.unsplash.com/photo-1574680096145-d05b474e2155?w=800',
+          'https://images.unsplash.com/photo-1571902943202-507ec2618e8f?w=800',
+          'https://images.unsplash.com/photo-1534438327276-14e5300c3a48?w=800',
+        ],
+        'isAd': true,
       },
       {
         'name': 'Jaime Santiago',
         'userImage': 'assets/images/professional.jpg',
-        'content': 'Just finished a meeting with my coleagues. Can say it was a great way to end the day!',
+        'content':
+            'Just finished a meeting with my coleagues. Can say it was a great way to end the day!',
         'likes': 6,
         'date': 'November 21',
         'contentImage': 'assets/images/meeting.png',
+        'isAd': false,
       },
       {
         'name': 'Mackenzie Iguiron',
@@ -63,6 +135,24 @@ class NewsfeedScreen extends StatelessWidget {
         'likes': 1,
         'date': 'October 10',
         'contentImage': null,
+        'isAd': false,
+      },
+      {
+        'name': 'Gourmet Bites',
+        'userImage': 'assets/images/food_logo.jpg',
+        'content':
+            'Delicious meals delivered to your door! Use code FEAST20 for 20% off.',
+        'likes': 743,
+        'date': 'Sponsored',
+        'contentImage': null,
+        'carouselImages': [
+          'https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=800',
+          'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?w=800',
+          'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=800',
+          'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?w=800',
+          'https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=800',
+        ],
+        'isAd': true,
       },
       {
         'name': 'Tyler',
@@ -71,6 +161,7 @@ class NewsfeedScreen extends StatelessWidget {
         'likes': 1001,
         'date': 'December 21',
         'contentImage': null,
+        'isAd': false,
       },
     ];
 
@@ -84,6 +175,10 @@ class NewsfeedScreen extends StatelessWidget {
           numOfLikes: posts[index]['likes'],
           date: posts[index]['date'],
           contentImage: posts[index]['contentImage'],
+          carouselImages: posts[index]['carouselImages'] != null
+              ? List<String>.from(posts[index]['carouselImages'])
+              : null,
+          isAdvertisement: posts[index]['isAd'] ?? false,
         );
       },
     );
